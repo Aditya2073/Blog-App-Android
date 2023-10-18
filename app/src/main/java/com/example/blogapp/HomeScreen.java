bd5267db963d006bdf4dce4e5263fe50c18197b8
Aditya Rasal <adityarasal@Adityas-MacBook-Pro.local>
2023-10-18 11:57:53 +0530

    First Commit

---
+++ app/src/main/java/com/example/blogapp/HomeScreen.java
@@ -0,0 +1,23 @@
+package com.example.blogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/blogapp/SplashScreen.java
@@ -0,0 +1,104 @@
+package com.example.blogapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.example.blogapp.databinding.SplashActivityBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class SplashScreen extends AppCompatActivity {
+
+    SplashActivityBinding binding;
+    GoogleSignInOptions signInOptions;
+    GoogleSignInClient signInClient;
+    FirebaseAuth auth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        }
+
+        binding = SplashActivityBinding.inflate(getLayoutInflater());
+        setupSignIn();
+    }
+
+    private void setupSignIn() {
+        auth = FirebaseAuth.getInstance();
+        signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.id.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        signInClient = GoogleSignIn.getClient(this, signInOptions);
+    }
+
+    @Override
+    protected void onStart() {
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            System.out.println("Home Page");
+        }else {
+            signIn();
+        }
+        super.onStart();
+    }
+
+    private void signIn() {
+        Intent intent = signInClient.getSignInIntent();
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                auth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SplashScreen.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(SplashScreen.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                });
+            }catch (ApiException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
